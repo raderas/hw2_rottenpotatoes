ef3e6f83a3105f384cef8a8cf4c5540fac26e81b
saasbook <saasbook@saasbook.(none)>
2012-08-11 00:24:50 -0700

    modified:   README 	modified:   app/controllers/movies_controller.rb 	modified:   app/models/movie.rb 	modified:   app/views/movies/index.html.haml

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,25 +1,45 @@
 class MoviesController < ApplicationController
-
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
-    # will render app/views/movies/show.<extension> by default
+  # will render app/views/movies/show.<extension> by default
   end
 
   def index
+
+    #Display rating checkboxes logic
+    @all_ratings = Movie.get_ratings
+    @selected_ratings = Hash.new
+
+    unless params[:ratings]==nil then
+      #Variable for handling ratings filtering
+      @rating_where_clause=Array.new
+      params[:ratings].each do |selected_rating,value|
+        @selected_ratings[selected_rating] = value
+        @rating_where_clause << selected_rating
+      end
+    end
+
+    #Header sorting and css formatting logic
     @title_header_class=nil
     @release_date_header_class=nil
+    
+    @order_string = ""
+    
     case params[:sort]
-    when "title" 
-      @movies = Movie.all(:order => "title ASC")
+    when "title"
+      @order_string = "title ASC"
+      #@movies = Movie.all(:order => "title ASC")
       @title_header_class = "hilite"
     when "release"
-      @movies = Movie.all(:order => "release_date ASC")
+      @order_string = "release_date ASC"
+      #@movies = Movie.all(:order => "release_date ASC")
       @release_date_header_class = "hilite"
-    else      
-    @movies = Movie.all
+    else
+    #@movies = Movie.all
     @sort = "none"
     end
+    @movies = Movie.find(:all,:order => @order_string, :conditions => {:rating => @rating_where_clause})
   end
 
   def new
