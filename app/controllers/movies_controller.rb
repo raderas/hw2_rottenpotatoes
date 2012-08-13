42eed8cce1596877cffc1bd63792749f43652008
Ubuntu <ubuntu@ip-10-248-24-205.us-west-2.compute.internal>
2012-08-13 14:28:41 +0000

    modified:   app/controllers/movies_controller.rb 	modified:   app/views/movies/index.html.haml

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,6 +11,41 @@ class MoviesController < ApplicationController
     @all_ratings = Movie.get_ratings
     @selected_ratings = Hash.new
 
+    
+    params_string = "\?redir=1"
+    self_redirect = false
+    #Reading :params from session hash if it exists and redirecting if needed
+    #debugger
+    unless session[:ratings]==nil then
+      if params[:ratings]==nil then
+        #debugger
+        session[:ratings].each {|hash_key,value| params_string << %q{&ratings[} << hash_key << %q{]=} << value }
+        #debugger
+        self_redirect = true
+        #redirect_to movies_path << params_string
+      else
+        session[:ratings] = params[:ratings]
+      end
+    end
+
+    #Reading :sort from session hash if it exists and redirecting if needed
+    #This should be reviewed because sort may not come from a refresh button
+    #push
+    unless session[:sort]==nil then
+      if params[:sort]==nil then
+        params_string << "\&sort=" << session[:sort]
+        self_redirect = true
+        else
+        session[:sort] = params[:sort]
+      end
+    end
+
+    #redirecting if needed
+    if self_redirect then
+      flash.keep
+      redirect_to movies_path << params_string
+    end
+
     unless params[:ratings]==nil then
       #Variable for handling ratings filtering
       @rating_where_clause=Array.new
@@ -18,26 +53,29 @@ class MoviesController < ApplicationController
         @selected_ratings[selected_rating] = value
         @rating_where_clause << selected_rating
       end
+      #saving :ratings on the session hash
+      session[:ratings] =  params[:ratings]
     end
 
     #Header sorting and css formatting logic
     @title_header_class=nil
     @release_date_header_class=nil
-    
-    @order_string = ""
-    
+
     case params[:sort]
     when "title"
       @order_string = "title ASC"
+      session[:sort] = params[:sort]
       #@movies = Movie.all(:order => "title ASC")
       @title_header_class = "hilite"
     when "release"
       @order_string = "release_date ASC"
+      session[:sort] = params[:sort]
       #@movies = Movie.all(:order => "release_date ASC")
       @release_date_header_class = "hilite"
     else
-    #@movies = Movie.all
-    @sort = "none"
+      #@movies = Movie.all
+      session[:sort] = "none"
+      @order_string = ""
     end
     @movies = Movie.find(:all,:order => @order_string, :conditions => {:rating => @rating_where_clause})
   end
